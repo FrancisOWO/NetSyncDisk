bf09b6c727bd909c4aef46658ae6d082c2bae3d0
FrancisOWO <francisowo@163.com>
2026-03-15 21:43:08 +0800

    update gitignore

---
+++ clean.bat
@@ -0,0 +1,3 @@
+@echo off
+call "D:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvarsall.bat" x64
+"D:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/bin/Hostx64/x64/nmake.exe" clean
